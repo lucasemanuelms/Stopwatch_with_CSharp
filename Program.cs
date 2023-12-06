782cc990419a1b9baaf820e68d8ae102293f1828
lucasemanuelms <lucas.ufc1310@gmail.com>
2023-12-06 09:24:52 -0300

    Menu and Start function done

--- Program.cs
+++ Program.cs
@@ -8,16 +8,29 @@ namespace StopWatch
     {
         static void Main(string[] args)
         {
-            Start(6);
+            Menu();
         }
 
         static void Menu(){
 
             Console.Clear();
-            Console.WriteLine("S = Segundo");
-            Console.WriteLine("M = Minuto");
+            Console.WriteLine("S = Segundo => 10s = 10 segundos");
+            Console.WriteLine("M = Minuto => 1m = 1 minuto");
             Console.WriteLine("0 = Sair");
             Console.WriteLine("Quanto tempo deseja contar?");
+
+            string data = Console.ReadLine().ToLower();
+            char type = char.Parse(data.Substring(data.Length-1,1));
+            int time = int.Parse(data.Substring(0, data.Length-1));
+            int multiplier = 1;
+
+            if(type == 'm')
+                multiplier = 60;
+
+            if(time == 0)
+                System.Environment.Exit(0);
+
+            Start(time * multiplier);
 
         }
 
@@ -35,6 +48,7 @@ namespace StopWatch
             Console.Clear();
             Console.WriteLine("StopWatch finalizado;");
             Thread.Sleep(2500);
+            Menu();
 
         }
     }
